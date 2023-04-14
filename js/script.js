c4723345936c23a431d4f4fdf1032d640aed1f5e
robertoalyssom <robert_alys@hotmail.com>
2023-04-14 18:59:59 -0300

    refac and reponsive banner sectio fixed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // sidebar
-const sidebarButton = document.querySelector('[data-sidebarButton]');
-const sidebar = document.querySelector('[data-sidebar]');
+const sidebarButton = document.querySelector("[data-sidebarButton]");
+const sidebar = document.querySelector("[data-sidebar]");
 
-sidebarButton.addEventListener("click", () => sidebar.classList.toggle("active"));
-
+sidebarButton.addEventListener("click", () =>
+  sidebar.classList.toggle("active")
+);
