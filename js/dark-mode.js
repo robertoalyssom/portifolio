237ae052014d6c211ef59e65c49f9990b3558c68
robertoalyssom <robert_alys@hotmail.com>
2023-05-19 12:52:08 -0300

    feat: add dark mode; style: in media queries for tablet view

---
+++ js/dark-mode.js
@@ -0,0 +1,62 @@
+const sunMoonButton = document.querySelectorAll("[data-darkLight-button]");
+const elements = document.querySelectorAll("[data-darkMode]");
+
+sunMoonButton.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+    changeMode(button);
+  });
+});
+
+function changeMode(button) {
+  changeIcon(button);
+
+  elements.forEach((element) => {
+    let datasetValue = element.dataset.darkmode;
+
+    if (element.tagName === "HEADER") {
+      changeImage(element);
+    }
+
+    bgBlack(element);
+
+    if (datasetValue === "card" || datasetValue === "footer") {
+      bgGrey(element);
+    }
+  });
+}
+
+function changeIcon(button) {
+  let iconElement = button.firstChild;
+  let elementFatherTag = button.parentNode.tagName;
+  let bodyClassListLength = document.body.classList.length;
+
+  // if clicked element's father is <nav> or <header>
+  if (bodyClassListLength === 0 && elementFatherTag === "NAV") {
+    iconElement.outerHTML = `<i class="fa-solid fa-sun navbar-icon"></i>`;
+  } else if (bodyClassListLength === 0) {
+    iconElement.outerHTML = `<i class="fa-solid fa-sun navegation-icon"></i>`;
+  } else {
+    iconElement.outerHTML = `<i class="fa-solid fa-moon navbar-icon"></i>`;
+  }
+}
+
+function changeImage(element) {
+  let logoImgElement = element.firstElementChild.firstElementChild.firstChild;
+  let containsDarkModeClassInBody =
+    document.body.classList.contains("dark-mode");
+
+  logoImgElement.src = "./img/logo/logo-preto.png";
+
+  if (containsDarkModeClassInBody) {
+    logoImgElement.src = "./img/logo/logo-branco.png";
+  }
+}
+
+function bgBlack(element) {
+  element.classList.toggle("dark-mode");
+}
+
+function bgGrey(element) {
+  element.classList.toggle("dark-mode__bg-grey");
+}
